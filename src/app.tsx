b08b4799e5e290dad41d29f7226a139e15f87236
edymusajev <edymusajev@gmail.com>
2023-12-26 09:24:33 +0700

    Update React component and CSS configuration

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
 import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.getElementById("root")!);
-root.render(<h2>Hello from React!</h2>);
+root.render(<h2 className="bg-red-400">Hello from React!</h2>);
